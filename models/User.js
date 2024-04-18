65e0ae3452e35990fbd0910d48919d78a71146df
sameer0288 <sameersheikh0288@gmail.com>
2024-04-18 17:31:32 +0530

    updated model and routes

--- models/User.js
+++ models/User.js
@@ -1,34 +1,87 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+const userSchema = new mongoose.Schema({
+  // User Information
+  userInformation: {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return emailRegex.test(value);
+        },
+        message: "Invalid email format",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  otp: {
-    type: Number,
-    required: true,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  location: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  workDetails: {
-    type: String,
+
+  // Verification Information
+  verificationInformation: {
+    otp: {
+      type: String,
+      default: null,
+    },
+    otpExpiresAt: {
+      type: Date,
+      default: null,
+    },
+    verificationToken: {
+      type: String,
+      default: null,
+    },
   },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+// Middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("userInformation.password")) return next();
+
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password with the salt
+    this.userInformation.password = await bcrypt.hash(
+      this.userInformation.password,
+      salt
+    );
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare hashed password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.userInformation.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
